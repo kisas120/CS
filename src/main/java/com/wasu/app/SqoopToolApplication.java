8e2bb8e2001437742ae806601f12844dd8014a86
1041711988@qq.com <zjy2561450>
2017-08-11 14:43:44 +0800

    cehsi 1

---
+++ src/main/java/com/wasu/app/SqoopToolApplication.java
@@ -0,0 +1,12 @@
+package com.wasu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqoopToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqoopToolApplication.class, args);
+	}
+}
